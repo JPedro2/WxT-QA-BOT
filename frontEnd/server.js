74f7eecbf6eef85b8fd2987e1ae281f2a31b3728
Joshua Dean <joshualukedean@gmail.com>
2020-04-11 16:40:24 +0100

    updated hostname in express code

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -6,10 +6,5 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-const server = app.listen(8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-  
-    console.log(`Example app listening at http://${host}:${port}`);
-  });
+app.listen(8080, '0.0.0.0');
+console.log('Server started');
