c352816db60ea51112e3452ca251620cfaba0824
Peter Morris <petmorri@cisco.com>
2020-04-14 17:05:07 +0000

    Fixed Flask API and changed front-end passphrase

--- backEnd/api.py
+++ backEnd/api.py
@@ -1,22 +1,30 @@
 import queries
 import flask
-from flask import request
-from flask import abort
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-app = flask.Flask(__name__)
 
+import sys
+# Set working directory
+sys.path.append('/home/WxT-QA-BOT')
+import credentials
+
+# Instantiate Flask app
+API_app = flask.Flask(__name__)
+
+# Instantiate limiter
 limiter = Limiter(
-    app,
+    API_app,
     key_func=get_remote_address,
     default_limits=["100 per day","10 per hour"]
 )
 
-@app.route('/')
+# Root
+@API_app.route('/')
 def default():
     return
 
-@app.route('/getAnswer', methods=['POST', 'GET'])
+# getAnswer by POST form
+@API_app.route('/getAnswer', methods=['POST', 'GET'])
 @limiter.limit("1 per minute", override_defaults=False)
 def getAnswer():
     question = request.form['question']
@@ -26,12 +34,15 @@ def getAnswer():
     except:
         abort(status=400)
 
-@app.route('/getAnswer/<question>')
+# getAnswer by GET
+@API_app.route('/getAnswer/<question>')
 @limiter.limit("1 per minute", override_defaults=False)
 def _getAnswer(question):
     return queries.getAnswer(question)
 
-@app.route('/addEntry', methods=['POST', 'GET'])
+# addEntry by POST form
+@API_app.route('/addEntry', methods=['POST', 'GET'])
+# Exempt from rate limit
 @limiter.exempt
 def addEntry():
     question = request.form['question']
@@ -41,3 +52,7 @@ def addEntry():
         return flask.Response(status=200)
     except:
         abort(status=400)
+
+# Main
+if __name__ == "__main__":
+    API_app.run(host=credentials.FLASK["Flask_HOST"], port=credentials.FLASK["Flask_PORT"], debug=False)
--- backEnd/queries.py
+++ backEnd/queries.py
@@ -1,15 +1,20 @@
 import mysql.connector
+import sys
+sys.path.append('/home/WxT-QA-BOT')
 import credentials
 
+# Connect to MySQL database
 db = mysql.connector.connect(
 	host=credentials.DB["host"],
 	user=credentials.DB["user"],
 	passwd=credentials.DB["passwd"],
-	database=credentials.DB["qanda"]
+	database=credentials.DB["database"]
 )
 
+# Instantiate cursor
 cursor = db.cursor()
 
+# Get an answer given a question
 def getAnswer(question):
 	stmt = ("SELECT answer, location FROM qanda WHERE question=%s")
 	data = (question)
@@ -18,6 +23,7 @@ def getAnswer(question):
 	answer, location = result[0], result[1]
 	return answer, location
 
+# Add an entry given a question, answer, and (optionally) a location
 def addEntry(question, answer, location=None):
 	try:
 		if location is not None:
