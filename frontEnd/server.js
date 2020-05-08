d119a0864d7dce5e38befc9e1ce7745177a589d3
Joshua Dean <joshuadean@joshuas-mbp.lan>
2020-05-08 10:56:06 +0100

    updated the express server

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -2,8 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/login.html'));
+});
+app.get('/main.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/main.html'));
+});
+app.get('/query.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/query.html'));
+});
+app.get('/update.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/update.html'));
 });
 
 app.listen(8080, '0.0.0.0');
