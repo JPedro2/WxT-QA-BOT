b3e7c6d2d40ff403f71aa6945fe40528609431ee
JPedro2 <peolivei@cisco.com>
2020-05-12 22:24:46 +0100

    Creating HTML directory and fixing wrong :PORT

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -5,19 +5,19 @@ var path = require('path');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/login.html'));
+    res.sendFile(path.join(__dirname + '/html/login.html'));
 });
 app.get('/main.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/main.html'));
+    res.sendFile(path.join(__dirname + '/html/main.html'));
 });
 app.get('/query.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/query.html'));
+    res.sendFile(path.join(__dirname + '/html/query.html'));
 });
 app.get('/update.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/update.html'));
+    res.sendFile(path.join(__dirname + '/html/update.html'));
 });
 app.get('/login.html', function(req, res){
-    res.sendFile(path.join(__dirname + '/login.html'));
+    res.sendFile(path.join(__dirname + '/html/login.html'));
 })
 
 app.listen(3006, '0.0.0.0');
--- frontEnd/update-frontEnd.sh
+++
@@ -1,7 +0,0 @@
-cd /home/WxT-QA-BOT/frontEnd
-rm -f index.html
-staticrypt main.html "WxT&IBM-BOT-Q@"
-mv main_encrypted.html index.html
-chmod 774 index.html
-cp /home/WxT-QA-BOT/frontEnd/index.html /var/www/html
-cp /home/WxT-QA-BOT/frontEnd/main.html /var/www/html
